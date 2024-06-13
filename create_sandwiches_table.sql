7c525329dfa0327aa06a0dce30f40ca790439055
brewchetta <chett.tiller@gmail.com>
2024-06-13 11:52:54 -0400

    reset for lecture, add license

--- create_sandwiches_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS sandwiches_table (
-id INTEGER PRIMARY KEY,
-name TEXT
-);
-
-INSERT INTO sandwiches_table (name) VALUES ("Cuban");
---
+++ create_tables.sql
@@ -0,0 +1,4 @@
+-- WE CAN USE THIS FILE TO CREATE OUR TABLES
+
+-- IN ORDER TO UTILIZE THE FILE, RUN THIS COMMAND (minus the --)
+    -- sqlite3 main.db < create_tables.sql
