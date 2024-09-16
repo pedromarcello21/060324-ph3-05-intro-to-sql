db0e55e278d651c63542c7cb9190c6b1edef7cd9
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-09-16 09:03:53 -0400

    latest

--- create_tables.sql
+++ create_tables.sql
@@ -2,3 +2,32 @@
 
 -- IN ORDER TO UTILIZE THE FILE, RUN THIS COMMAND (minus the --)
     -- sqlite3 main.db < create_tables.sql
+
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    age NUMBER, 
+    salary NUMBER
+
+);
+
+--CREATE - INSERT
+
+--- read with SELECT
+
+--ALTER TABLE to add or drop columns
+
+--UPDATE....SET...WHERE... to make updates
+
+--DELETE FROM
+
+
+CREATE TABLE cats(
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    age NUMBER,
+    cuteness NUMBER
+);
